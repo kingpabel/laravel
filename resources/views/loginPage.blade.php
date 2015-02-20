ee948377112cfcc51e9db513c8b61534aef9d9a0
kingpabel <imtiazpabel@yahoo.com>
2015-02-21 05:00:02 +0600

    single from multi login

--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -1,27 +1,12 @@
 @extends('login')
 
 @section('content')
-<div class="col-md-6">
-        {!! Form::open(array('role' => 'form', 'accept-charset' => 'utf-8', 'class' => 'form-signin', 'url' => 'login/check-company')) !!}
-        <h2 class="form-signin-heading">Employer/Admin Login</h2>
-        <div class="login-wrap">
-            <div class="user-login-info">
-                <div class="input">
-                    <input type="text" name="company_user_name" class="form-control" placeholder="Username" autofocus="" required>
-                </div>
-                <div class="input">
-                    <input type="password" name="password" class="form-control" placeholder="Password" required>
-                </div>
-            </div>
-            <label class="checkbox">
-            </label>
-            <button type="submit" class="btn btn-lg btn-login btn-block">Sign in</button>
-        </div>
-        </form>
+<div class="col-md-3">
+
 </div>
 <div class="col-md-6">
     {!! Form::open(array('role' => 'form', 'accept-charset' => 'utf-8', 'class' => 'form-signin', 'url' => 'login/check-user')) !!}
-        <h2 class="form-signin-heading">Employee Login</h2>
+        <h2 class="form-signin-heading">Login</h2>
         <div class="login-wrap">
             <div class="user-login-info">
                 <div class="input text required">
@@ -32,7 +17,7 @@
                 </div>            </div>
             <label class="checkbox">
             </label>
-            <button type="submit" class="btn btn-lg btn-login btn-block">Sign in</button>        </div>
+            <button type="submit" class="btn btn-lg btn-login btn-block">Login</button>        </div>
     </form>
 </div>
 
