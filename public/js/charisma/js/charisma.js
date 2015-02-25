80c3656f4e1371d927186a3197b4b49804d972ac
kingpabel <imtiazpabel@yahoo.com>
2015-02-26 03:08:09 +0600

    charisma js error solve

--- public/js/charisma/js/charisma.js
+++ public/js/charisma/js/charisma.js
@@ -311,7 +311,7 @@ function docReady(){
 
 
 	//initialize the calendar
-	$('#calendar').fullCalendar({
+	/*$('#calendar').fullCalendar({
 		header: {
 			left: 'prev,next today',
 			center: 'title',
@@ -320,29 +320,29 @@ function docReady(){
 		editable: true,
 		droppable: true, // this allows things to be dropped onto the calendar !!!
 		drop: function(date, allDay) { // this function is called when something is dropped
-		
+
 			// retrieve the dropped element's stored Event Object
 			var originalEventObject = $(this).data('eventObject');
-			
+
 			// we need to copy it, so that multiple events don't have a reference to the same object
 			var copiedEventObject = $.extend({}, originalEventObject);
-			
+
 			// assign it the date that was reported
 			copiedEventObject.start = date;
 			copiedEventObject.allDay = allDay;
-			
+
 			// render the event on the calendar
 			// the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
 			$('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-			
+
 			// is the "remove after drop" checkbox checked?
 			if ($('#drop-remove').is(':checked')) {
 				// if so, remove the element from the "Draggable Events" list
 				$(this).remove();
 			}
-			
+
 		}
-	});
+	});*/
 	
 	
 	//chart with points
