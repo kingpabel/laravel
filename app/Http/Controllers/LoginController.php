c505c39d8e6ec2eecfc119ddfcf0e1b3ae60e1ac
kingpabel <imtiazpabel@yahoo.com>
2015-03-01 02:50:01 +0600

    company created from controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,6 +1,12 @@
 <?php namespace App\Http\Controllers;
+use App\Company;
+use App\User;
 use Auth;
 use Request;
+use Session;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller {
@@ -33,6 +39,59 @@ class LoginController extends Controller {
         else
         {
             return redirect('/');
+        }
+    }
+
+    public function anyCreateCompany()
+    {
+        if(Request::all()){
+            $rules = array(
+                'company_name' => 'required',
+                'username'=> "required|unique:users,username|alpha_dash",
+                'password'  => 'required|min:6|max:10',
+                'company_email' => 'required|email',
+                'phone' => 'required',
+                'time_zone' => 'required'
+            );
+            /* Laravel Validator Rules Apply */
+            $validator = Validator::make(Input::all(), $rules);
+            if ($validator->fails()):
+                $validationError =  $validator->messages()->first();
+                Session::flash('flashError', $validationError);
+                return redirect('login/create-company');
+            else:
+                $userCreate = new \App\User();
+                $userCreate->username = trim(Request::input('username'));
+                $userCreate->user_label = 1;
+                $userCreate->password = Hash::make(trim(Request::input('password')));
+                $userCreate->save();
+
+                $companyCreate = new Company();
+                $companyCreate->company_name = trim(Request::input('company_name'));
+                $companyCreate->company_email = trim(Request::input('company_email'));
+                $companyCreate->phone = trim(Request::input('phone'));
+                $companyCreate->time_zone = trim(Request::input('time_zone'));
+                $companyCreate->user_id = $userCreate->id;
+                $companyCreate->save();
+
+                $userUpdate = User::find($userCreate->id);
+                $userUpdate->company_id = $companyCreate->id;
+                $userUpdate->save();
+            endif;
+            Session::flash('flashSuccess', 'Welcome in Here.You are now a Company.Create User and Manage Them!!');
+            $credentials = array(
+                'username' => Request::input('username'),
+                'password' => Request::input('password'),
+            );
+            if(Auth::attempt( $credentials ))
+            {
+                if(Auth::user()->user_label == 2)
+                    return redirect()->intended('user');
+                if(Auth::user()->user_label == 1)
+                    return redirect()->intended('company');
+            }
+        }else {
+            return view('createCompany');
         }
     }
 }
