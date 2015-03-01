a70ec759ba0443798c20153a5213c6aec38b161e
kingpabel <imtiazpabel@yahoo.com>
2015-03-02 04:03:48 +0600

    rox to kingpabel

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -37,7 +37,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="<?php // echo base_url()?>">ROX ATN</a>
+                <a class="navbar-brand" href="<?php // echo base_url()?>">Kingpabel ATN</a>
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
