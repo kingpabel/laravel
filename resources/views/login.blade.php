950b933d1df97606bcabada6712f83b8f031431c
kingpabel <imtiazpabel@yahoo.com>
2015-03-03 01:48:15 +0600

    analytics js added to login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,18 +12,18 @@
             {!! HTML::style('css/bootstrap3/css/bootstrap.min.css') !!}
             {!! HTML::style('css/bootstrap3/css/font-awesome.min.css') !!}
             {!! HTML::style('css/bootstrap3/css/main.css') !!}
-            <script type="text/javascript" language="javascript">
-                <script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-                ga('create', 'UA-60038966-1', 'auto');
-                ga('send', 'pageview');
+            ga('create', 'UA-60038966-1', 'auto');
+            ga('send', 'pageview');
 
-            </script>
-        function resultDelete()
+        </script>
+        <script type="text/javascript" language="javascript">
+            function resultDelete()
             {
                 var chk=confirm("Are you sure to delete this?");
                 if(chk)
