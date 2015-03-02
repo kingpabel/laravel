270ed021d480ab414a9d4e7d361f18cced85a73e
kingpabel <imtiazpabel@yahoo.com>
2015-03-03 02:10:22 +0600

    update profile loader done

--- resources/views/Users/updateProfile.blade.php
+++ resources/views/Users/updateProfile.blade.php
@@ -47,6 +47,8 @@
                             <input type="text" required class="input-xlarge input" name="user_email" value="<?php echo Auth::user()->user_email?>" id="user_email" placeholder="User Email">
                         </div>
                     </div>
+                    <div id="loader">
+                    </div>
                     <div class="form-actions">
                         <button type="submit" class="btn btn-success">Update</button>
                     </div>
@@ -67,7 +69,11 @@
                     type: "POST",
                     data: values,
                     cache: false,
+                    beforeSend: function(){
+                        $('#loader').html('<img src="{{ URL::to('images/loader_gif.gif') }}" style="height: 100px;margin-left: 100px;">');
+                    },
                     success: function(data) {
+                        $('#loader').hide();
                         if(data=='true') {
                             $.pnotify({
                                 title: 'Success',
