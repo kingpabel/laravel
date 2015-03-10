77de362315f4a10b766c0ff29d5455c4afab0644
kingpabel <imtiazpabel@yahoo.com>
2015-03-11 04:17:21 +0600

    Minutes

--- resources/views/Company/home.blade.php
+++ resources/views/Company/home.blade.php
@@ -100,7 +100,7 @@
     <div class="row-fluid">
         <div class="box span12">
             <div class="box-header well">
-                <h2><i class="icon-info-sign"></i>Hours Worked Today</h2>
+                <h2><i class="icon-info-sign"></i>Minutes Worked Today</h2>
                 <div class="box-icon">
                     <a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
                     <a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
