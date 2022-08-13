0a8e7fca5ef9340dfe4efd34e669db9dceee1742
kingpabel <ippabel@gmail.com>
2022-08-14 01:12:27 +0600

    test code modified

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
@@ -13,6 +12,6 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true()
     {
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 }
