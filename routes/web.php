756c949198cdf792c01fdf16febf5b1618266525
kingpabel <ippabel@gmail.com>
2022-08-29 10:50:03 +0600

    check by throwing exception

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,6 @@ Route::get('/', function () {
     $logger = $logging->psrLogger('hello-app-name');
     $logger->info('log from stack driver');
 
+    throw new Exception('Error Processing Request', 1);
     return view('welcome');
 });
